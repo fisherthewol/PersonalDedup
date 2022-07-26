19a437355ddbcd4faf44b94145973f7738a459ad
George Kokinis <george.kokinis@gmail.com>
2022-07-26 01:18:37 +0100

    Rename test function.

--- PersonalDedupeTest/ImageDedupUnitTest.cs
+++ PersonalDedupeTest/ImageDedupUnitTest.cs
@@ -5,13 +5,13 @@ namespace PersonalDedupeTest;
 public class ImageDedupUnitTest
 {
     /// <summary>
-    /// Tests that two known-different images are marked as non-duplicate.
+    /// Tests that two known-different images get a likelihood of 1 for same extension.
     /// </summary>
     /// <param name="image1">Filename of first image to test.</param>
     /// <param name="image2">Filename of second image to test.</param>
     [Theory]
     [InlineData("HNI_0063.JPG", "HNI_0064.JPG")]
-    public void KnownDifferentImagesArentDuplicate(string image1, string image2)
+    public void KnownDifferentImagesAreExtensionEqual(string image1, string image2)
     {
         FileInfo fiImage1 = new FileInfo(image1);
         FileInfo fiImage2 = new FileInfo(image2);
