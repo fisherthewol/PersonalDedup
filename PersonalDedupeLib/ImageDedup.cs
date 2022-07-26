aeaa8df888656437f2cfdf53d64e706f290463bf
George Kokinis <george.kokinis@gmail.com>
2022-07-26 01:02:06 +0100

    Refactor function to return a "likelyhood" that it's a dupe.

--- PersonalDedupeLib/ImageDedup.cs
+++ PersonalDedupeLib/ImageDedup.cs
@@ -8,13 +8,14 @@ public class ImageDedup
     /// <param name="image1">First image to test.</param>
     /// <param name="image2">Second image to test.</param>
     /// <returns>True if duplicate; false otherwise.</returns>
-    public static bool IsImageDuplicate(FileInfo image1, FileInfo image2)
+    public static int ImagesLikelyDuplicate(FileInfo image1, FileInfo image2)
     {
         if (!image1.Exists || !image2.Exists) throw new ArgumentException("Image does not exist.");
+        int isLikelyDupe = 0;
         // Check length is same:
-        if (image1.Length != image2.Length) return false;
+        if (image1.Length == image2.Length) isLikelyDupe++;
         // Check creation time:
-        if (image1.CreationTimeUtc != image2.CreationTimeUtc) return false;
-        return true;
+        if (image1.CreationTimeUtc != image2.CreationTimeUtc) isLikelyDupe++;
+        return isLikelyDupe;
     }
 }
